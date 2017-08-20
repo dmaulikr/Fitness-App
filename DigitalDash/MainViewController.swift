e0b5c1786b79248871afae7a2341858dba73d0d6
Nicholas Blackburn <nicholasblackburn@Nicholass-MacBook-Pro.local>
2017-08-20 07:41:02 -0700

    front page portrait mode only

--- DigitalDash/MainViewController.swift
+++ DigitalDash/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     /********************************************************************************
@@ -27,6 +28,14 @@ class MainViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
     }
 
 
